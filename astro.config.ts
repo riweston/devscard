2215642fb18674e96807955ef6f284deefd930d5
Richard Weston <github@riweston.io>
2024-02-09 15:26:30 +0000

    ci: Build github pages

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,8 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://riweston.github.io',
+  base: '/devscard/',
   integrations: [tailwind(), image(), compress()],
   vite: {
     plugins: [visualizer()],
